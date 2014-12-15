ae14a7c25e073b031941478f3456a417ff22dffa
ItsJonQ <itsjonq@gmail.com>
2014-12-14 20:27:39 -0500

    Started migrating lumberjack class from lumberjack theme to it's own plugin

---
+++ lumberjack.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Lumberjack
+Plugin URI: https://github.com/ItsJonQ/lumberjack
+Description: A Wordpress plugin that extends the Timber plugoin
+Author: Q
+Version: 0.0.1
+Author URI: https://github.com/ItsJonQ/
+*/
+
+global $lumberjack;
+
+class Lumberjack {
+
+}
+
+$lumberjack = new Lumberjack();
+$GLOBALS['lumberjack'] = $lumberjack;
